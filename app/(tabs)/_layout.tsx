521b852f8c451d06abbf1fc79313e33224f448f9
edson2001 <djiedson413@gmai.com>
2023-03-10 16:14:02 +0100

    fix: adiciona um novo modelo de organização para as stacks

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,24 @@
 import React from "react"
-import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView, View, ScrollView, Image, TouchableOpacity, FlatList} from "react-native";
 import {Feather} from "@expo/vector-icons"
 import {Tabs} from "expo-router"
 
 
 export default function Layout() {
   return (
-    <Tabs screenOptions={{ headerShown: false, headerStyle:{backgroundColor: "#202020"}}}>
-        <Tabs.Screen name="home"/>
-        <Tabs.Screen name="send" />
-        <Tabs.Screen name="card" />
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarStyle:{
+        backgroundColor: "#202020",
+      },
+      tabBarShowLabel: false
+    }}>
+      <Tabs.Screen name="home/index" options={{
+        tabBarIcon: ()=> (<Feather name="home" color={"#FFF"} size={20} />)
+      }} />
+      <Tabs.Screen name="send/index" options={{
+        tabBarIcon: ()=> (<Feather name="send" color={"#FFF"} size={20} />),
+        tabBarShowLabel: false
+      }}  /> 
     </Tabs>
   );
 }
