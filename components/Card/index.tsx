46f6c116b5a8bb28fb087c1e8b4e5185aa1353a0
edson2001 <djiedson413@gmai.com>
2023-03-11 11:14:21 +0100

    fix: muda aparencia do cartão

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
-import CardImage from "@Images/Card.png"
-import CardChip from "@Images/Group.png"
 import Visa from "@Images/Group1.png"
-
+import {Feather} from "@expo/vector-icons"
 import { StyledComponent } from 'nativewind';
 
 const Card: React.FC = () => {
   return (
-    <StyledComponent className="relative w-[282px] h-[180px] flex-col justify-between p-3 ml-2" component={View} > 
-       <Image source={CardImage} className="w-[282px] h-[180px] absolute" />
-       <Image source={CardChip} />
-       <Text className="text-[#FFF] font-bold text-[16px]">AOA 123,876,087,00</Text>
+    <StyledComponent className="relative bg-[#4F339A] rounded-[10px] w-full h-[180px] flex-col justify-between p-3" component={View} > 
+       
+       
+       <View>
+            <View className="flex-row justify-between">
+                <Text className="text-white font-bold text-[25px] mb-[12px]">Saldo</Text>
+                <TouchableOpacity>
+                    <Feather name="toggle-left" color={"#FFF"} size={40} />
+                </TouchableOpacity>
+            </View>
+            <View className="flex-row">
+                <Text className="text-white font-bold text-[16px] mr-[10px]">AOA</Text>
+                <Text className="text-white font-medium text-[16px]">123,876,087.00</Text>
+            </View>
+       </View>
 
-        <View className="flex-row justify-between">
+        <View className="flex-row justify-between items-center">
 
             <View>
-                <Text className="text-[#FFF] font-bold text-[9px]">Card Holder name</Text>
-                <Text className="text-[#FFF] font-bold">Edson Bandola</Text>
+                <Text className="text-white font-semibold text-[12px]">Número Do Cartão</Text>
+                <Text className="text-white font-medium text-[10px]">1234 4567 6553 1389</Text>
             </View>
 
-            <View className="flex-row items-center">
-                <View className="mr-2"> 
-                    <Text className="text-[#FFF] text-[9px] ">Expiry Date</Text>
-                    <Text className="text-[#FFF] text-[12px] font-bold">02/27</Text>
-                </View>
-                <View>
-                    <Image source={Visa} />
-                </View>
-            </View>
-
+            <Image source={Visa} />
         </View>
 
     </StyledComponent>
---
+++ components/Icon/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+
+const Icon: React.FC = () => {
+  return (
+    <Image source={require("@Images/Vector.svg")} />
+  );
+}
+
+export default Icon;
