effc92cc0a76a6efa0ffe049681bc1dca03014fd
edson2001 <djiedson413@gmai.com>
2023-03-11 12:03:29 +0100

    feat: página de finalização de pagamentos

---
+++ app/(tabs)/done/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const done: React.FC = () => {
+  return <View />;
+}
+
+export default done;
