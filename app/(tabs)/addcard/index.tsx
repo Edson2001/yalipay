3b1b01df2fc50f91d8d1582c7caac49fb6573c1d
edson2001 <djiedson413@gmai.com>
2023-03-11 13:09:21 +0100

    fix: altera texto do cartão

--- app/(tabs)/addcard/index.tsx
+++ app/(tabs)/addcard/index.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, SafeAreaView, View, ScrollView, Image} from "react-native";
+import Input from "@Components/Input";
 
-// import { Container } from './styles';
+import ButtonLink from '@Components/ButtonLink';
 
-const addcard: React.FC = () => {
-  return <View />;
+export default function addcard() {
+  return (
+    <ScrollView className="bg-background p-7">
+      <StatusBar  style="light"  />
+      <SafeAreaView className="flex-col justify-around h-screen">
+        <View>
+          <Text className="text-white font-bold text-[15px]">YaliPay</Text>
+          <Text className="text-white font-bold text-[24px] mt-10 mb-[32px]">Add Cartão</Text>
+          <Input label="Número do Cartão" classInput="mb-[30px]" />
+        </View>
+
+        <View>
+          <ButtonLink link="/donecard" text="Confirmar" classButton="mb-[12px] bg-buttonColor" />
+          <Text className="text-center text-white font-bold text-[13px]">Cancelar Operação</Text>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
 }
-
-export default addcard;
