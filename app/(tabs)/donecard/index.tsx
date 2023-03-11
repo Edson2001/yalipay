3ac5259169db424e3fc8e039ea064adc641516fe
edson2001 <djiedson413@gmai.com>
2023-03-11 13:07:17 +0100

    feat: adiciona página final do cartão

---
+++ app/(tabs)/donecard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const donecard: React.FC = () => {
+  return <View />;
+}
+
+export default donecard;
