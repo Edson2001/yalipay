0590f6c87ffc90e0e25c70ac6731346a208637a4
edson2001 <djiedson413@gmai.com>
2023-03-11 11:38:54 +0100

    fix: altera textos

--- app/(tabs)/send/index.tsx
+++ app/(tabs)/send/index.tsx
@@ -1,30 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView, View, ScrollView, Image, TouchableOpacity, FlatList} from "react-native";
-
-import {Feather} from "@expo/vector-icons"
-import Card from "@Components/Card"
-import Item from "@Components/Item";
+import { Text, SafeAreaView, View, ScrollView, Image} from "react-native";
 import Input from "@Components/Input";
 import Button from "@Components/Button";
 export default function Send() {
   return (
     <ScrollView className="bg-background p-7">
-     
-        <StatusBar  style="light"  />
-      
-        <SafeAreaView className="flex-col justify-around h-screen">
-           
-            <View>
-                <Text className="text-white font-bold text-3xl mt-10 mb-[62px]">Transferir</Text>
-                <Input label="IBAN" classInput="mb-[30px]" />
-                <Input label="Montante"  />
-            </View>
+      <StatusBar  style="light"  />
+      <SafeAreaView className="flex-col justify-around h-screen">
+        <View>
+          <Text className="text-white font-bold text-[15px]  mb-10">YaliPay</Text>
+          <Text className="text-white font-bold text-[24px] mt-10 mb-[62px]">Transferir</Text>
+          <Input label="IBAN" classInput="mb-[30px]" />
+          <Input label="Montante"  />
+        </View>
 
-            <View>
-                <Button onClick={()=> {}} text="Transferir" classButton="mb-[12px]" />
-                <Text className="text-center text-white font-bold text-[13px]">Cancelar Operação</Text>
-            </View>
-        </SafeAreaView>
+        <View>
+          <Button onClick={()=> {}} text="Transferir" classButton="mb-[12px]" />
+          <Text className="text-center text-white font-bold text-[13px]">Cancelar Operação</Text>
+        </View>
+      </SafeAreaView>
     </ScrollView>
   );
 }
