2b5b532d6015b71662fb3cafb5c7426c893a0b83
edson2001 <djiedson413@gmai.com>
2023-03-14 06:36:44 +0100

    fix: muda orientação do scroll da flatlist

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -6,17 +6,17 @@ import Item from "@Components/Item";
 
 export default function Home() {
   return (
-    <SafeAreaView className="bg-background p-5">
+    <SafeAreaView className="bg-background p-5 flex-col">
       <StatusBar  style="light"  />
       <Text className="text-white font-bold text-[15px] mt-10 mb-10">YaliPay</Text>
       <Card />
       <View className="flex-row mt-[45px]  mb-[25px]">
-          <Text className="text-white font-bold text-[13px]  mr-[4px]">Movimentos </Text>
-          <Text className="text-white font-light text-[13px]  ">Recentes</Text>
-        </View>
-      <ScrollView>
-        <FlatList data={[1, 2, 3, 5, 6, 7, 8, 9, 10]} renderItem={()=> (<Item />)} />
-      </ScrollView>
+        <Text className="text-white font-bold text-[13px]  mr-[4px]">Movimentos </Text>
+        <Text className="text-white font-light text-[13px]  ">Recentes</Text>
+      </View>
+    
+      <FlatList scrollEnabled={true} data={[1, 2, 3, 5, 6, 7, 8, 9, 10]} renderItem={()=> (<Item />)} />
+      
     </SafeAreaView>
   );
 }
