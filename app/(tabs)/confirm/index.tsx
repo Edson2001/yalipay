fa22f3389c034e4dd433f089486b9e153fbc28a9
edson2001 <djiedson413@gmai.com>
2023-03-17 15:02:27 +0100

    feat: adiciona componente de OTP

--- app/(tabs)/confirm/index.tsx
+++ app/(tabs)/confirm/index.tsx
@@ -2,11 +2,8 @@ import React, {useState, useRef} from 'react';
 import { StatusBar } from "expo-status-bar";
 import { View, Text, ScrollView, SafeAreaView, TouchableOpacity , TextInput} from 'react-native';
 import ButtonLink from "@Components/ButtonLink"
-
+import OPT from '@Components/OPT';
 const confirm: React.FC = () => {
-
-  const [pin1, setPin1] = useState(null)
-  const refPin2 = useRef(null)
 
 
   return (
@@ -19,35 +16,13 @@ const confirm: React.FC = () => {
           <Text className="text-white mb-[40px]">
             Para validar a transferência, no valor de AOA 79,45, insira o código de confirmação que recebeu por SMS.
           </Text>
-
-          <View className="flex-row justify-between mb-[37px]">
-            <View className="w-[76px]">
-              <TextInput onChange={(pin1)=>{
-                setPin1(pin1)
-
-                if(pin1 != null){
-                  refPin2.current.focus()
-                }
-
-              }} maxLength={1} className="border-b-2 border-white text-white font-bold text-[32px] text-center" />
-            </View>
-            <View className="w-[76px]">
-              <TextInput ref={refPin2} maxLength={1} className="border-b-2 border-white text-white font-bold text-[32px] text-center" />
-            </View>
-            <View className="w-[76px]">
-              <TextInput maxLength={1} className="border-b-2 border-white text-white font-bold text-[32px] text-center" />
-            </View>
-            <View className="w-[76px]">
-              <TextInput maxLength={1} className="border-b-2 border-white text-white font-bold text-[32px] text-center" />
-            </View>
-          </View>
+          
+          <OPT />
           <TouchableOpacity className="flex-row">
-              <Text className="text-white">Não recebi o código de confirmação, R</Text>
-              <Text className="text-buttonColor">eenviar</Text>
+            <Text className="text-white">Não recebi o código de confirmação, R</Text>
+            <Text className="text-buttonColor">eenviar</Text>
           </TouchableOpacity>
         </View>
-
-        
 
         <View>
           <ButtonLink onClick={()=> {}} text="Confirmar" link='/done' classButton="mb-[12px] bg-buttonColor" />
