0bcef9e55b8983aed37ceec0a0d1da0ba8e30e3b
edson2001 <djiedson413@gmai.com>
2023-03-22 09:43:57 +0100

    feat: adiciona o componente de container

--- app/(tabs)/cards/index.tsx
+++ app/(tabs)/cards/index.tsx
@@ -1,32 +1,25 @@
 import React from 'react';
-import { StatusBar } from "expo-status-bar";
-import { View, Text, ScrollView, SafeAreaView, TouchableOpacity } from 'react-native';
+
+import { View, Text, ScrollView } from 'react-native';
 import {Feather} from "@expo/vector-icons"
 import Card from "@Components/Card"
-
+import Container from '@Components/Container';
 import { Link } from 'expo-router';
 
 const cards: React.FC = () => {
   return (
-    <SafeAreaView className="bg-background p-7">
-      <StatusBar  style="light"  />
-      <SafeAreaView className="flex-col h-screen">
-        <View>
-          <Text className="text-white font-bold text-[15px]  mb-[40px] mt-[40px]">YaliPay</Text>
-          <View className="flex-row items-center justify-between">
-            <Text className="text-white font-bold text-[24px] mb-[30px]">Cartões</Text>
-            <Link href="/addcard" >
-              <Feather name="file-plus" size={20} color="#FFF" />
-            </Link>
-          </View>
-        </View>
-
-        <ScrollView>
-          <Card />
-          <Card color="bg-[#ED1E79]" />
-        </ScrollView>
-      </SafeAreaView>
-    </SafeAreaView>
+    <Container>
+      <View className="flex-row items-center justify-between">
+        <Text className="text-white font-bold text-[24px] mb-[30px]">Cartões</Text>
+        <Link href="/addcard" >
+          <Feather name="file-plus" size={20} color="#FFF" />
+        </Link>
+      </View>
+      <ScrollView className='h-full'>
+        <Card />
+        <Card color="bg-[#ED1E79]" />
+      </ScrollView>
+    </Container>
   );
 }
 
