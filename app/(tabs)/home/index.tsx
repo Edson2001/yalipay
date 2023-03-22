3dd8af6a0cd847448a204166d0234d0af0e5cc2b
edson2001 <djiedson413@gmai.com>
2023-03-22 09:43:44 +0100

    feat: adiciona o componente de container

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,22 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView, View, ScrollView, Image, TouchableOpacity, FlatList} from "react-native";
 
+import { Text, View, FlatList} from "react-native";
+import Container from "@Components/Container";
 import Card from "@Components/Card"
 import Item from "@Components/Item";
 
 export default function Home() {
   return (
-    <SafeAreaView className="bg-background p-5 flex-col">
-      <StatusBar  style="light"  />
-      <Text className="text-white font-bold text-[15px] mt-10 mb-10">YaliPay</Text>
+    <Container>
       <Card />
       <View className="flex-row mt-[45px]  mb-[25px]">
         <Text className="text-white font-bold text-[13px]  mr-[4px]">Movimentos </Text>
         <Text className="text-white font-light text-[13px]  ">Recentes</Text>
       </View>
-    
       <FlatList scrollEnabled={true} data={[1, 2, 3, 5, 6, 7, 8, 9, 10]} renderItem={()=> (<Item />)} />
-      
-    </SafeAreaView>
+    </Container>
   );
 }
