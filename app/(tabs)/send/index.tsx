71c583c824aec950057ce7063a6a412fc2100ff8
edson2001 <djiedson413@gmai.com>
2023-03-22 11:16:52 +0100

    fix: muda tamanho do botão

--- app/(tabs)/send/index.tsx
+++ app/(tabs)/send/index.tsx
@@ -6,13 +6,12 @@ import ButtonLink from '@Components/ButtonLink';
 export default function Send() {
   return (
     <Container>
-      <View className={`flex-col  items-center h-full justify-around `}>
+      <View className={`flex-col items-center h-full justify-around mt-[-42px] `}>
         <View className="w-full">
           <Text className="text-white font-bold text-[24px]">Transferir </Text>
           <Input label="IBAN" classInput="w-full" />
           <Input label="Montante"  />
         </View>
-
         <View className="w-full">
           <ButtonLink link="/confirm" text="Transferir" classButton="bg-buttonColor" />
           <Text className="text-center text-white font-bold text-[13px]">Cancelar Operação</Text>
