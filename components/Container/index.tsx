8feee26c61c8f1caac8c97623a314689e7620bc5
edson2001 <djiedson413@gmai.com>
2023-03-22 10:46:08 +0100

    fix: adiciona tamanho da com base no tamanho do telefone

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { View, SafeAreaView, Text } from 'react-native';
+import { View, SafeAreaView, Text, useWindowDimensions } from 'react-native';
 import { StatusBar } from "expo-status-bar";
 
 interface Props{
     children?: React.ReactComponentElement
 }
 const Container: React.FC<Props> = (props) => {
-  return (
-    <SafeAreaView className="bg-background h-full">
-        <StatusBar  style="light"  />
-        <View className='container pt-10 pl-[19px] pr-[19px]'>
-            <Text className="text-white font-bold text-[15px] mb-[40px]">YaliPay</Text>
-            {props.children}
-        </View>
-    </SafeAreaView>
-  );
+    const windowHeight = useWindowDimensions().height
+    const windowWidth = useWindowDimensions().width
+    return (
+        <SafeAreaView className={`bg-background w-[${windowWidth.toFixed()}px] h-[${windowHeight.toFixed()}px]`}>
+            <StatusBar  style="light"  />
+            <View className='container pl-[19px] pr-[19px]'>
+                <Text className="text-white font-bold text-[15px]">YaliPay {windowHeight.toFixed()} </Text>
+                {props.children}
+            </View>
+        </SafeAreaView>
+    );
 }
 
 export default Container;
