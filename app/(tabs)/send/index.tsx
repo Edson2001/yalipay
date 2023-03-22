3a7b293893023942a7103f22b802b9ddd13dbb46
edson2001 <djiedson413@gmai.com>
2023-03-22 09:44:03 +0100

    feat: adiciona o componente de container

--- app/(tabs)/send/index.tsx
+++ app/(tabs)/send/index.tsx
@@ -1,26 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { Text, SafeAreaView, View, ScrollView, Image} from "react-native";
+import { Text, View} from "react-native";
 import Input from "@Components/Input";
-
+import Container from "@Components/Container";
 import ButtonLink from '@Components/ButtonLink';
 
 export default function Send() {
   return (
-    <ScrollView className="bg-background p-7">
-      <StatusBar  style="light"  />
-      <SafeAreaView className="flex-col justify-around h-screen">
-        <View>
-          <Text className="text-white font-bold text-[15px]  mb-10">YaliPay</Text>
-          <Text className="text-white font-bold text-[24px] mt-10 mb-[62px]">Transferir</Text>
-          <Input label="IBAN" classInput="mb-[30px]" />
+    <Container>
+      <View className="flex-col  h-full items-center">
+        <View className="w-full">
+          <Text className="text-white font-bold text-[24px]">Transferir</Text>
+          <Input label="IBAN" classInput="w-full" />
           <Input label="Montante"  />
         </View>
 
-        <View>
-          <ButtonLink link="/confirm" text="Transferir" classButton="mb-[12px] bg-buttonColor" />
+        <View className="w-full">
+          <ButtonLink link="/confirm" text="Transferir" classButton="bg-buttonColor" />
           <Text className="text-center text-white font-bold text-[13px]">Cancelar Operação</Text>
         </View>
-      </SafeAreaView>
-    </ScrollView>
+      </View>
+    </Container>
   );
 }
